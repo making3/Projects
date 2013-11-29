00127234a54df1a4a5aa8a8d84e07d62298bb6da
making3 <mattking73@gmail.com>
2013-11-28 20:07:55 -0600

    Finished change return.

--- numbers/change_return.py
+++ numbers/change_return.py
@@ -1,25 +1,32 @@
 import collections
-word = float(raw_input('Enter the cost of money to give change for: '))
-#word = 5.31
-change = collections.OrderedDict([ ('dollar', 1.00), ('quarter', 0.25), ('dime', 0.10), ('nickel', 0.05), ('penny', 0.01) ])
-output = {}
+money = float(raw_input('Enter the cost of money to give change for: '))
+change = collections.OrderedDict([ 
+    ('One-Hundred', 100.00),
+    ('Fifty', 50.00),
+    ('Twenty', 20.00),
+    ('Ten', 10.00),
+    ('Five', 5.00),
+    ('One', 1.00), 
+    ('Quarter', 0.25), 
+    ('Dime', 0.10), 
+    ('Nickel', 0.05), 
+    ('Penny', 0.01) ])
 
-def printChange(remainder, changeIndex):
-    if changeIndex >= len(change):
-        output['remainder'] = int(remainder);
-        return remainder
-        
-    changeInput = change.values()[changeIndex] 
-    output[change.keys()[changeIndex]] = int(remainder // changeInput)
-    changeIndex += 1
-    print 'Remainder: ' + str(remainder)
-    print 'Change Input: ' + str(changeInput)
-    print str(int(remainder % changeInput))
-    return printChange(remainder % changeInput, changeIndex)
+def calcChange(remainder, index = 0):
+    coinFactor = change.values()[index]
+    coinCount = int(remainder / coinFactor)
+    remainder = round(remainder % coinFactor, 2)
+
+    output = {}
+    if (index + 1) < len(change):
+        output = calcChange(remainder, index + 1)
+
+    output[change.keys()[index]] = coinCount
+    return output
 
 def printOutput(outputArray):
     for value in outputArray:
         print value + ': ' + str(outputArray[value])
 
-printChange(word, 0)
-printOutput(output)
+result = calcChange(money)
+printOutput(result)
